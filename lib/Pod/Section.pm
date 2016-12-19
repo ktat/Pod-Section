b5414d1b76bc0621e5b3af15625dacc98113a621
ktat <ktat.is@gmail.com>
2016-12-20 01:10:11 +0900

    Checking in changes prior to tagging of version 0.02.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index cd729f2..581ad77 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,11 @@
     Revision history for Pod-Section
    
    +{{$NEXT}}
    +
    +0.02 2016-12-19T16:10:08Z
    +	fix typo.
    +        Thanks Arthur Axel 'fREW' Schmidt.
    +
     0.01    2010/05/03 18:29:00
             First version.

--- lib/Pod/Section.pm
+++ lib/Pod/Section.pm
@@ -106,11 +106,11 @@ Pod::Section - select specified section from Module's POD
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 
 =head1 SYNOPSIS
